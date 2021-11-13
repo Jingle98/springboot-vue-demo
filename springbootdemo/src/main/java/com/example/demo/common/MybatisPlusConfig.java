f6143e5bd74d60b9af1c48f0fd6b9d935b666269
Jingle98 <sunhikingjing@gmail.com>
2021-11-13 15:50:01 +0800

    配置myBatis的插件

--- springbootdemo/src/main/java/com/example/demo/common/MybatisPlusConfig.java
+++ springbootdemo/src/main/java/com/example/demo/common/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.common;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version
  **/
 @Configuration
+@MapperScan("com.example.demo.mapper")
 public class MybatisPlusConfig {
     /**
      * 分页插件
