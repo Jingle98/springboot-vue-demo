c5fda6972933cd1506290a54f87c79fe406e1ba0
Jingle98 <sunhikingjing@gmail.com>
2021-11-13 15:46:39 +0800

    配置myBatis的插件

--- springbootdemo/src/main/java/com/example/demo/common/MybatisPlusConfig.java
+++ springbootdemo/src/main/java/com/example/demo/common/MybatisPlusConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.common;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,8 +18,12 @@ public class MybatisPlusConfig {
     /**
      * 分页插件
      */
+    // 最新版
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor(){
-        return new PaginationInnerInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
+
 }
