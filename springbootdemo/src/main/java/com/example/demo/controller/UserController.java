d435f567ddbdc7bfbdbec72fe1cf08239891c71c
Jingle98 <sunhikingjing@gmail.com>
2021-11-13 16:06:06 +0800

    mapper

---
+++ springbootdemo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.common.Result;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author Jingnan
+ * @Date 2021/11/13 15:51
+ * @Version
+ **/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    UserMapper userMapper;
+
+    @PostMapping
+    public Result<?> save(@RequestBody User user){
+        userMapper.insert(user);
+        return Result.success();
+    }
+}
---
+++ springbootdemo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.demo.entity.User;
+
+/**
+ * @InterfaceName UserMapper
+ * @Description 写一下接口
+ * @Author Jingnan
+ * @Date 2021/11/13 16:00
+ * @Version
+ **/
+public interface UserMapper extends BaseMapper<User> {
+}
