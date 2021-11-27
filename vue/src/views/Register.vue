7aaba45509c71e7bcf38ce29c69baf5e5ffc0fd4
Jingle98 <sunhikingjing@gmail.com>
2021-11-28 00:21:00 +0800

    加入校验

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,16 +1,19 @@
 <template>
   <div style="width: 100%; height: 100vh;background-color: darkslateblue;overflow: hidden">
     <div style="width: 400px; margin: 150px auto">
-        <div style="color:#cccccc; font-size: 30px; text-align: center;padding: 30px 0; ">欢迎登录</div>
+        <div style="color:#cccccc; font-size: 30px; text-align: center;padding: 30px 0; ">欢迎注册</div>
         <el-form ref="form" :model="form" size="normal" :rules="rules">
-          <el-form-item  prop="username">
+          <el-form-item prop="username">
             <el-input prefix-icon="el-icon-user-solid" v-model="form.username" placeholder="请输入账号"></el-input>
           </el-form-item>
           <el-form-item prop="password">
             <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password placeholder="请输入密码"></el-input>
           </el-form-item>
+          <el-form-item prop="confirm">
+            <el-input prefix-icon="el-icon-lock" v-model="form.password" show-password placeholder="请输入密码"></el-input>
+          </el-form-item>
           <el-form-item >
-            <el-button style="width: 100%;" type="primary" @click="login">登录</el-button>
+            <el-button style="width: 100%;" type="primary" @click="login">注册</el-button>
           </el-form-item>
 
         </el-form>
@@ -26,30 +29,41 @@ export default {
   data() {
     return {
       form:{},
-      rules:{
-        username:[
+      rules: {
+        username: [
           {required: true, message: '请输入用户名', trigger: 'blur'},
         ],
         password: [
           {required: true, message: '请输入密码', trigger: 'blur'},
         ],
-      },
+        confirm: [
+          {required: true, message: '请确认密码', trigger: 'blur'},
+        ],
+      }
     }
   },
   methods:{
-    login() {
+    register() {
+      if (this.form.password !== this.form.confirm) {
+        this.$message({
+          type: "error",
+          message: '2次密码输入不一致！'
+        })
+        return
+      }
+
       this.$refs['form'].validate((valid) => {
         if (valid) {
-          request.post("/api/user/login", this.form).then(res => {
-            debugger
+          request.post("/api/user/register", this.form).then(res => {
+
             if (res.code === '0') {
               debugger
               this.$message({
                 type: "success",
-                message: "登录成功"
+                message: "注册成功"
               })
               // sessionStorage.setItem("user", JSON.stringify(res.data))  // 缓存用户信息
-              this.$router.push("/")  //登录成功之后进行页面的跳转，跳转到主页
+              this.$router.push("/login")  //登录成功之后进行页面的跳转，跳转到登录界面
             } else {
               debugger
               this.$message({
@@ -60,6 +74,7 @@ export default {
           })
         }
       })
+
 
     }
   }
